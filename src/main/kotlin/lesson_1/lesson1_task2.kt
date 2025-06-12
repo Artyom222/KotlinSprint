599b0ce9554f2b9c097cee09d0a2abf4d0868177
APecherkin <pech.artyom2@yandex.ru>
2025-06-12 16:44:17 +0500

    KS-1-2 data output

--- src/main/kotlin/lesson_1/lesson1_task2.kt
+++ src/main/kotlin/lesson_1/lesson1_task2.kt
@@ -1,2 +1,15 @@
 package org.example.lesson_1
 
+val numberOfOrder: Int = 75
+val thanksText:String = "Благодарим за покупку!"
+
+fun main(){
+    println("Количество заказов: $numberOfOrder")
+    println(thanksText)
+
+    var numberOfWorkers = 2000
+//    println("Количество работников: $numberOfWorkers")
+    numberOfWorkers = numberOfWorkers - 1
+    println("Количество работников: $numberOfWorkers")
+
+}
