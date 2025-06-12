2deb8aad200a79efec24a90d7b6a38016c7184d5
APecherkin <pech.artyom2@yandex.ru>
2025-06-12 17:11:18 +0500

    KS-1-3 space flight

--- src/main/kotlin/lesson_1/lesson1_task3.kt
+++ src/main/kotlin/lesson_1/lesson1_task3.kt
@@ -1,2 +1,14 @@
 package org.example.lesson_1
+val year = 1961
+var hour = 9
+var minute = 7
+
+fun main(){
+    println("Взлет:\nГод:$year \nЧас:0$hour \nМинут:0$minute")
+    println()
+    hour = 10
+    minute = 55
+    println("Время посадки:$hour:$minute")
+
+}
 
