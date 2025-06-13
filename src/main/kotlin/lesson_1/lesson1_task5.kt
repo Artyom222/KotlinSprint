097a5a26606315e31afa27c24b364ac3f7fd60b8
APecherkin <pech.artyom2@yandex.ru>
2025-06-13 16:29:12 +0500

    KS-1-5 add const, add String.format

--- src/main/kotlin/lesson_1/lesson1_task5.kt
+++ src/main/kotlin/lesson_1/lesson1_task5.kt
@@ -1,12 +1,15 @@
 package org.example.lesson_1
-var second = 6480
+
+const val SECOND_IN_MINUTE = 60
+const val MINUTE_IN_HOUR = 60
 
 fun main(){
-    var minute = second / 60 //делим нацело
-    second = (second - minute * 60) //из первонаяального количества секунд отнимаем количество минут = остаток
-    val hour = minute / 60
-    minute = minute - hour * 60
-//Переводим числа в строковый тип и добавляем 0, пока длина строки не станет 2
-    println("${hour.toString().padStart(2, '0')}:${minute.toString().padStart(2, '0')}:${second.toString().padStart(2, '0')}")
+    var second = 6480
+    var minute = second / SECOND_IN_MINUTE
+    second = second % SECOND_IN_MINUTE
+    val hour = minute / MINUTE_IN_HOUR
+    minute = minute % MINUTE_IN_HOUR
+
+    println("%02d:%02d:%02d".format(hour, minute, second))
 
 }
