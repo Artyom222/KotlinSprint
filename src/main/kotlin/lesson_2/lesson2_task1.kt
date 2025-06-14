c4433c983aaf2540cecc6b33f1f9468a3acb9de5
APecherkin <pech.artyom2@yandex.ru>
2025-06-14 19:38:11 +0500

    KS-2-1 salary Fund

--- src/main/kotlin/lesson_2/lesson2_task1.kt
+++ src/main/kotlin/lesson_2/lesson2_task1.kt
@@ -1,10 +1,7 @@
 package org.example.lesson_2
 
-fun main(){
+fun main() {
     val grade = listOf(3, 4, 3, 5)
-//    val numberOfStudent = grade.size.toFloat()
-//    val arithmeticMean1 = grade.sum() / numberOfStudent
-    val arithmeticMean2 = grade.average()
-    println(arithmeticMean2)
-
+    val arithmeticMean = grade.average()
+    println("%.2f".format(arithmeticMean))
 }
