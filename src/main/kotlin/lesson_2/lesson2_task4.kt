3d2871b37a988cc7a9342e3ff0ac26fe5febdaee
APecherkin <pech.artyom2@yandex.ru>
2025-06-16 21:22:58 +0500

    KS-2-4 smth

--- src/main/kotlin/lesson_2/lesson2_task4.kt
+++ src/main/kotlin/lesson_2/lesson2_task4.kt
@@ -1,13 +1,15 @@
 package lesson_2
 
+const val PERCENTAGE_IN_FRACTIONS = 100
+
 fun main() {
-    val buffOnLoot = 0.2
+    val buffOnLoot = 20
     val minedCrystalOre = 7
     val minedIronOre = 11
-    val bonusCrystalOre = minedCrystalOre * buffOnLoot
-    val bonusIronOre = minedIronOre * buffOnLoot
+    val bonusCrystalOre = minedCrystalOre * buffOnLoot / PERCENTAGE_IN_FRACTIONS
+    val bonusIronOre = minedIronOre * buffOnLoot / PERCENTAGE_IN_FRACTIONS
 
-    println("Бонусная кристаллическая руда:${bonusCrystalOre.toInt()}")
-    println("Бонусная железная руда:${bonusIronOre.toInt()}")
+    println("Бонусная кристаллическая руда:${bonusCrystalOre}")
+    println("Бонусная железная руда:${bonusIronOre}")
 
 }
