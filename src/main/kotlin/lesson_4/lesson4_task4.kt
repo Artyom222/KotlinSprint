2f02f3b67fb2036afee8a7da840f770ffd2e03cf
APecherkin <pech.artyom2@yandex.ru>
2025-06-28 20:25:05 +0500

    KS-4-4 smth

--- src/main/kotlin/lesson_4/lesson4_task4.kt
+++ src/main/kotlin/lesson_4/lesson4_task4.kt
@@ -2,22 +2,12 @@ package org.example.lesson_4
 
 fun main() {
     val trainingDay = 5
-    val exercisesForHand: Boolean
-    val exercisesForLeg: Boolean
-    val exercisesForBack: Boolean
-    val exercisesForPress: Boolean
+    val isEven = trainingDay % 2 == 0
 
-    if (trainingDay % 2 == 0) {
-        exercisesForHand = false
-        exercisesForLeg = true
-        exercisesForBack = true
-        exercisesForPress = false
-    } else {
-        exercisesForHand = true
-        exercisesForLeg = false
-        exercisesForBack = false
-        exercisesForPress = true
-    }
-
-    println("Упражнения для рук: $exercisesForHand \nУпражнения для ног: $exercisesForLeg \nУпражнения для спины: $exercisesForBack \nУпражнения для пресса: $exercisesForPress")
+    println("""
+        Упражнения для рук: ${!isEven}
+        Упражнения для ног: $isEven
+        Упражнения для спины: $isEven
+        Упражнения для пресса: ${!isEven}
+    """.trimIndent())
 }
