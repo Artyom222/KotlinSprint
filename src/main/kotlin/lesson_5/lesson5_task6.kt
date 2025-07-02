d8e48f0af232be5adc2b20dfd0a5960d9ad0bc33
APecherkin <pech.artyom2@yandex.ru>
2025-07-02 20:42:18 +0500

    KS-5-6 bmi

--- src/main/kotlin/lesson_5/lesson5_task6.kt
+++ src/main/kotlin/lesson_5/lesson5_task6.kt
@@ -1,19 +1,20 @@
 package org.example.lesson_5
 import kotlin.math.pow
+const val CENTIMETERS_TO_METERS = 100
 
 fun main() {
     print("Введите вес в килграммах: ")
     val userWeight = readln().toFloat()
     print("Введите рост в сантиметрах: ")
     val userHeightCentimetre = readln().toFloat()
-    val userHeightMetre = userHeightCentimetre / 100
+    val userHeightMetre = userHeightCentimetre / CENTIMETERS_TO_METERS
 
     val bodyMassIndex = userWeight / userHeightMetre.pow(2)
 
     when {
         bodyMassIndex < 18.5 -> println("Недостаточная масса тела")
-        (18.5 <= bodyMassIndex && bodyMassIndex < 25) -> println("Нормальная масса тела")
-        (25 <= bodyMassIndex && bodyMassIndex < 30) -> println("Избыточная масса тела")
+        bodyMassIndex < 25 -> println("Нормальная масса тела")
+        bodyMassIndex < 30 -> println("Избыточная масса тела")
         bodyMassIndex >= 30 -> println("Ожирение")
     }
     println("Индекс массы тела = %.2f".format(bodyMassIndex))
