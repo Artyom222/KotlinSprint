7c8cff1d6778ce1acb3ddd337624f55078eff747
APecherkin <pech.artyom2@yandex.ru>
2025-07-06 19:26:28 +0500

    KS-6-5 smth

--- src/main/kotlin/lesson_6/lesson6_task5.kt
+++ src/main/kotlin/lesson_6/lesson6_task5.kt
@@ -2,24 +2,29 @@ package org.example.lesson_6
 
 fun main() {
     var attemptCount = 3
-    var isBot = true
-    var numbers: List<Int>
-    var answer: Int
+
     println("Подтвердите, что вы не робот")
 
-    while (isBot && attemptCount > 0) {
-        numbers = List(2) {(0..9).random()}
+    while (attemptCount > 0) {
+        val numbers = List(2) { (0..9).random() }
         print("Решите пример ${numbers[0]} + ${numbers[1]} = ")
-        answer = readln().toInt()
+        val answer = readln().toInt()
         --attemptCount
-        if (answer == (numbers[0] + numbers[1])) {
-            isBot = false
-            println("Доступ открыт")
-        } else if (attemptCount != 0) {
-            println("Неверно. Попробуйте еще раз")
-        } else {
-            println("Доступ закрыт!")
+
+        when {
+            answer == (numbers[0] + numbers[1]) -> {
+                println("Доступ открыт")
+                break
+            }
+
+            attemptCount != 0 -> {
+                println("Неверно. Попробуйте еще раз")
+            }
+
         }
+    }
+    if (attemptCount == 0) {
+        println("Доступ закрыт!")
     }
 
 }
