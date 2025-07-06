86d768c19f4d071d0f5600d03c733545d9b3af23
APecherkin <pech.artyom2@yandex.ru>
2025-07-06 16:44:17 +0500

    KS-6-4 smth

--- src/main/kotlin/lesson_6/lesson6_task4.kt
+++ src/main/kotlin/lesson_6/lesson6_task4.kt
@@ -2,18 +2,16 @@ package org.example.lesson_6
 
 fun main() {
     val winNumber = (1..9).random()
-    var isWin = false
     var attemptCount = 5
-    var number: Int
 
-    while (!isWin && attemptCount > 0) {
+    while (attemptCount > 0) {
         print("Введите число: ")
-        number = readln().toInt()
+        val number = readln().toInt()
         --attemptCount
 
         if (number == winNumber) {
             println("Поздравляем! Вы выиграли!")
-            isWin = true
+            break
         } else {
             println("Вы не угадали. Осталось $attemptCount опыток.")
         }
