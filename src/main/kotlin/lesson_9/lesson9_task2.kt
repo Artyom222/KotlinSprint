6bc485ec9f9332469aa67e90d0069c1cfeb601f1
APecherkin <pech.artyom2@yandex.ru>
2025-07-13 09:45:33 +0500

    KS-9-2 smth

---
+++ src/main/kotlin/lesson_9/lesson9_task2.kt
@@ -0,0 +1,16 @@
+package org.example.lesson_9
+
+fun main() {
+    val baseListIngredients = mutableListOf("яйцо", "сыр", "колбаса")
+    println("В рецепте есть базовые ингредиенты: ${baseListIngredients.toString()}")
+
+    println("Желаете добавить еще?")
+    val answer = readln()
+
+    if (answer.equals("да", true )) {
+        println("Какой ингредиент вы хотите добавить?")
+        val newIngredient = readln()
+        val newListIngredients = baseListIngredients + newIngredient
+        println("Теперь в рецепте есть следующие ингредиенты: ${newListIngredients.toString()}")
+    }
+}
