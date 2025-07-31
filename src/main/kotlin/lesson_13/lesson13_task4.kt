aeef402d7fa964ea00ccf47d3a0f4a7da9f9eb18
APecherkin <pech.artyom2@yandex.ru>
2025-07-31 21:33:31 +0500

    KS-13-4

---
+++ src/main/kotlin/lesson_13/lesson13_task4.kt
@@ -0,0 +1,41 @@
+package org.example.lesson_13
+
+import kotlin.collections.mutableListOf
+
+class PhoneContact1(
+    val name: String,
+    val phoneNumber: Long,
+    val company: String? = null
+) {
+    fun printContact() {
+        println("Имя: $name \nНомер: $phoneNumber \nКомпания: ${company ?: "<не указано>"}\n")
+    }
+}
+
+fun main() {
+    val phoneBook = mutableListOf<PhoneContact1>()
+
+    inputContact(phoneBook)
+    phoneBook.forEach { it.printContact() }
+}
+
+fun inputContact(phoneBook: MutableList<PhoneContact1>) {
+    print("Введите имя контакта: ")
+    val nameContact = readln()
+
+    print("Введите номер телефона контакта: ")
+    val phoneContact = readln().toLongOrNull()
+
+    print("Введите название компании: ")
+    var companyContact: String? = readln()
+    if (companyContact == "") {
+        companyContact = null
+    }
+
+    if (phoneContact != null) {
+        val newContact = PhoneContact1(nameContact, phoneContact, companyContact)
+        phoneBook.add(newContact)
+    } else {
+        println("Номер телефона не введен. Котакт не будет создан")
+    }
+}
