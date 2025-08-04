8f826dd52a983105124cac306476f2ed1b49e3a3
APecherkin <pech.artyom2@yandex.ru>
2025-08-04 21:20:01 +0500

    KS-14-1

--- src/main/kotlin/lesson_14/lesson14_task1.kt
+++ src/main/kotlin/lesson_14/lesson14_task1.kt
@@ -5,28 +5,27 @@ open class Liner(
     open val maxSpeed: Int = 30,
     open val capacity: Int = 1000,
     open val loadCapacity: Int = 200,
-    open val canBreakIce: Boolean = false
-    )
+    open val canBreakIce: Boolean = false,
+)
 
 class CargoShip(
     name: String,
     override val maxSpeed: Int = 15,
     override val capacity: Int = 100,
     override val loadCapacity: Int = 1000,
-    override val canBreakIce: Boolean = false
-): Liner(name)
+    override val canBreakIce: Boolean = false,
+) : Liner(name)
 
 class Icebreaker(
     name: String,
     override val maxSpeed: Int = 15,
     override val capacity: Int = 100,
     override val loadCapacity: Int = 100,
-    override val canBreakIce: Boolean = true
-): Liner(name)
+    override val canBreakIce: Boolean = true,
+) : Liner(name)
 
 fun main() {
     val liner = Liner("Титаник")
     val cargo = CargoShip("Восток")
     val iceBreaker = Icebreaker("Полярный")
-
 }
