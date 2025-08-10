d33af7a240d29f5facfd9589095609b510747c34
APecherkin <pech.artyom2@yandex.ru>
2025-08-10 17:48:43 +0500

    KS-15-1

---
+++ src/main/kotlin/lesson_15/lesson15_task1.kt
@@ -0,0 +1,36 @@
+package org.example.lesson_15
+
+interface Swimmable {
+    fun swim() {
+        println("(${javaClass.simpleName})Я могу плавать!")
+    }
+}
+
+interface Flyable {
+    fun fly() {
+        println("(${javaClass.simpleName})Я могу летать!")
+    }
+}
+
+class Crucian : Swimmable
+
+class Seagull : Flyable, Swimmable
+
+class Duck : Flyable, Swimmable
+
+fun main() {
+    val crucian = Crucian()
+    val seagull = Seagull()
+    val duck = Duck()
+
+    crucian.swim()
+    seagull.swim()
+    seagull.fly()
+    duck.fly()
+    duck.swim()
+
+}
+
+
+
+
