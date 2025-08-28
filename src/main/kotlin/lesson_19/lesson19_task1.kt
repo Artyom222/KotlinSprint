98c851be0dc8c523fa35ad604c463d1fc1d50646
APecherkin <pech.artyom2@yandex.ru>
2025-08-28 20:59:52 +0500

    KS-19-1

--- src/main/kotlin/lesson_19/lesson19_task1.kt
+++ src/main/kotlin/lesson_19/lesson19_task1.kt
@@ -1,14 +1,13 @@
 package org.example.lesson_19
 
-enum class Fishes {
-    GUPPY,
-    ANGELFISH,
-    GOLDFISH,
-    SIAMESE_FIGHTING_FISH,
+enum class Fishes(val fishName: String) {
+    GUPPY("гуппи"),
+    ANGELFISH("скалярия"),
+    GOLDFISH("золотая рыбка"),
+    SIAMESE_FIGHTING_FISH("петушок"),
 }
 
 fun main() {
     println("Пользователь может добавить следующих рыб:")
-    for (i in Fishes.entries) println(i)
-
+    for (i in Fishes.entries) println(i.fishName)
 }
