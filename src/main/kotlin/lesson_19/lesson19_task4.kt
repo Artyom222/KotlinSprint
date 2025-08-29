a6bb2cb834586d808275168ae4ca684a37387dfc
APecherkin <pech.artyom2@yandex.ru>
2025-08-29 21:31:28 +0500

    KS-19-4

---
+++ src/main/kotlin/lesson_19/lesson19_task4.kt
@@ -0,0 +1,44 @@
+package org.example.lesson_19
+
+class Tank(
+    val model: String,
+    val mass: Int,
+    val maxSpeed: Int,
+    val armorThickness: Int,
+    var ammoCount: Int,
+    var tankAmmo: Ammo? = null,
+) {
+    fun shot() {
+        if (tankAmmo != null) {
+            println("Выстрел снарядом ${tankAmmo!!.color}.\nНанесено ${tankAmmo!!.damage} урона")
+            ammoCount--
+        } else {
+            println("Ошибка! Нет снаряда!")
+        }
+    }
+
+    fun equipAmmo(ammo: Ammo) {
+        println("Вы подобрали снаряд: ${ammo.color}")
+        ammoCount++
+        tankAmmo = ammo
+    }
+}
+
+enum class Ammo(
+    val damage: Int,
+    val color: String,
+) {
+    BLUE(5, "синий"),
+    GREEN(10, "зеленый"),
+    RED(20, "красный");
+}
+
+fun main() {
+    val tank = Tank("T-90", 3000, 25, 30, 0)
+
+    tank.shot()
+    tank.equipAmmo(Ammo.BLUE)
+    tank.shot()
+    tank.equipAmmo(Ammo.RED)
+    tank.shot()
+}
