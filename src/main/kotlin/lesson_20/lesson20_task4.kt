5f113aab51729a9b6500ba121b3065aef6aeacb7
APecherkin <pech.artyom2@yandex.ru>
2025-09-14 19:59:05 +0500

    KS-20-4

--- src/main/kotlin/lesson_20/lesson20_task4.kt
+++ src/main/kotlin/lesson_20/lesson20_task4.kt
@@ -1,14 +1,14 @@
 package org.example.lesson_20
 
 fun main() {
-    val listString = listOf<String>("Меню","Настройки","Поиск","Контакты","О нас")
+    val items = listOf<String>("Меню", "Настройки", "Поиск", "Контакты", "О нас")
 
-    val listLambda = listString.map { item->
+    val listLambda = items.map { item ->
         { println("Нажат элемент [$item]") }
     }
 
     listLambda.forEachIndexed { index, lambda ->
-        if (index % 2 == 0) {
+        if ((index + 1) % 2 == 0) {
             lambda()
         }
     }
