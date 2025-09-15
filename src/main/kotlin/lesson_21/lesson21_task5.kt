4d9efa080aaf169e67366bfb1937d4220f64a970
APecherkin <pech.artyom2@yandex.ru>
2025-09-15 18:32:45 +0500

    KS-21-5

--- src/main/kotlin/lesson_21/lesson21_task5.kt
+++ src/main/kotlin/lesson_21/lesson21_task5.kt
@@ -1,17 +1,11 @@
 package org.example.lesson_21
 
 fun Map<String, Int>.maxSkill(): String {
-    var maxValue = 0
-    this.forEach {
-        if (it.value > maxValue) {
-            maxValue = it.value
-        }
-    }
-    return this.entries.first { it.value == maxValue }.key
+    return this.maxBy { it.value }.key
 }
 
 fun main() {
-    val skills = mapOf("Выпад" to 12, "Взмах" to 54, "Разворот 360" to 119, "Метание кинжала" to 120)
+    val skills = mapOf("Выпад" to 12, "Взмах" to 54, "Разворот 360" to 120, "Метание кинжала" to 120)
 
     println(skills.maxSkill())
 }
